d47588ae31efb8dfd50e2bd645515ecbc360f55e
yinping <yp_ren@hotmail.com>
2016-09-12 19:51:31 +0800

    ...

---
+++ src/openwrt/install.xx-net.sh
@@ -0,0 +1,29 @@
+# /bin/sh
+
+opkg install wget ca-certificates unzip
+
+UPD_URL='https://github.com/XX-net/XX-Net/raw/master/code/default/update_version.txt'
+UPD_FILE=${UPD_URL##*/}
+wget -O ${UPD_FILE} ${UPD_URL}
+
+ZIP_URL=`sed -n '/Stable/{n;p}' ${UPD_FILE}`
+VERSION=${ZIP_URL##*/}
+VERSION=${VERSION%%\ *}
+UNPACK_DIR=XX-Net-${VERSION}
+wget -O ${VERSION} ${ZIP_URL}
+rm -rf ${UNPACK_DIR}
+unzip ${VERSION}
+
+LOCAL_DIR=code/${VERSION}/gae_proxy/local
+NOARCH_DIR=code/${VERSION}/python27/1.0/lib/noarch
+rm -rf   xx-net/${LOCAL_DIR}
+rm -rf   xx-net/${NOARCH_DIR}
+mkdir -p xx-net/${LOCAL_DIR%/*}
+mkdir -p xx-net/${NOARCH_DIR%/*}
+mv ${UNPACK_DIR}/code/default/gae_proxy/local         xx-net/${LOCAL_DIR%/*}/
+mv ${UNPACK_DIR}/code/default/python27/1.0/lib/noarch xx-net/${NOARCH_DIR%/*}/
+
+rm -rf ${UPD_FILE}
+rm -rf ${VERSION}
+rm -rf ${UNPACK_DIR}
+
